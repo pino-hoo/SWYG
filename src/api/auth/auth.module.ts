dee9a1a31ef0a49ab6c8b5ede45e932aed313cce
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-28 01:39:36 +0900

    feat : Add Auth Module

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -20,11 +20,12 @@ import User from 'src/api/auth/domain/user.entity'
 
 // ** Custom Module Imports
 import AuthController from 'src/api/auth/controller/auth.controller'
-import AuthService from 'src/api/auth/service/auth.service'
 import UserRepository from 'src/api/auth/repository/user.repository'
 import MateModule from 'src/api/mate/mate.module'
 import { male } from 'src/config/env/node'
 import TypeOrmExModule from 'src/common/repository/typeOrmEx.module'
+import UserService from 'src/api/auth/service/user.service'
+import AuthService from './service/auth.service'
 
 @Module({
   imports: [
@@ -51,12 +52,11 @@ import TypeOrmExModule from 'src/common/repository/typeOrmEx.module'
     TypeOrmModule.forFeature([User]),
     TypeOrmExModule.forCustomRepository([UserRepository]),
     JwtModule.registerAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_ACCESS_TOKEN_SECRET'),
         signOptions: {
-          expiresIn: configService.get('JWT_EXPIRESIN'),
+          expiresIn: config.get('JWT_ACCESS_TOKEN_EXPIRATION_TIME'),
         },
       }),
     }),
@@ -64,6 +64,7 @@ import TypeOrmExModule from 'src/common/repository/typeOrmEx.module'
   ],
   controllers: [AuthController],
   providers: [
+    UserService,
     AuthService,
     LocalStrategy,
     JwtStrategy,
