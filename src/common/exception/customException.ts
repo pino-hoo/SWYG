54a5b748041fdaa2cb27afd2894ed3c1315ec398
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-27 23:27:17 +0900

    feat : Add Custom Exception

---
+++ src/common/exception/customException.ts
@@ -0,0 +1,24 @@
+import {
+  HttpException,
+  HttpStatus,
+  BadRequestException as BadRequest,
+  NotFoundException as NotFound,
+} from '@nestjs/common'
+
+export class NotFoundException extends NotFound {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export class BadRequestException extends BadRequest {
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export class InternalServerErrorException extends HttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.INTERNAL_SERVER_ERROR)
+  }
+}
---
+++ src/common/exception/excepitionMessageEnum.ts
@@ -0,0 +1,5 @@
+enum ExceptionMessage {
+  NOT_FOUND_USER = '유저를 찾을 수 없습니다.',
+}
+
+export default ExceptionMessage
